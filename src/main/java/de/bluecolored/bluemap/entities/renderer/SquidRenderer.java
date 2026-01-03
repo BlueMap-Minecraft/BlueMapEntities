0cf7dadfe707d86fa543d3f389884a54bf641148
miraculixx <miraculixxyt@gmail.com>
2026-01-03 23:52:50 +0100

    Remove unsued imports

--- src/main/java/de/bluecolored/bluemap/entities/renderer/SquidRenderer.java
+++ src/main/java/de/bluecolored/bluemap/entities/renderer/SquidRenderer.java
@@ -34,7 +34,6 @@ import de.bluecolored.bluemap.core.resources.pack.resourcepack.model.Model;
 import de.bluecolored.bluemap.core.util.Key;
 import de.bluecolored.bluemap.core.world.Entity;
 import de.bluecolored.bluemap.core.world.block.BlockNeighborhood;
-import de.bluecolored.bluemap.entities.entity.AgeEntity;
 import de.bluecolored.bluemap.entities.entity.GlowSquid;
 import de.bluecolored.bluemap.entities.entity.Squid;
 
